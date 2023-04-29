924db121037a99a69cd7b08b9a051d13492a5157
walter <arenalwm@gmail.com>
2023-04-28 22:53:38 -0300

    todolistapp ready to start

---
+++ todoList2/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './board/board'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Board brand={"To do list"} />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
